6b1fbe1c0fb18ad7c8d9d0f6a4dd77968b2319ed
Tim van Dijen <tvdijen@gmail.com>
2022-01-08 20:13:01 +0100

    Add some use-statements

--- hooks/hook_configpage.php
+++ hooks/hook_configpage.php
@@ -1,15 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
+use SimpleSAML\Locale\Translate;
+use SimpleSAML\Module;
+use SimpleSAML\XHTML\Template;
+
 /**
  * Hook to add the memcacheMonitor module to the config page.
  *
  * @param \SimpleSAML\XHTML\Template &$template The template that we should alter in this hook.
  */
-function memcacheMonitor_hook_configpage(\SimpleSAML\XHTML\Template &$template): void
+function memcacheMonitor_hook_configpage(Template &$template): void
 {
-    $template->data['links']['memcacheMonitor'] = [
-        'href' => SimpleSAML\Module::getModuleURL('memcacheMonitor/memcachestat.php'),
-        'text' => \SimpleSAML\Locale\Translate::noop('Memcache statistics'),
+    $template->data['links'][] = [
+        'href' => Module::getModuleURL('memcacheMonitor/memcachestat.php'),
+        'text' => Translate::noop('Memcache statistics'),
     ];
+
     $template->getLocalization()->addModuleDomain('memcacheMonitor');
 }
